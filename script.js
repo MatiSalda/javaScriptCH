59b1c6319097875850932633887931cdbe1dff6b
Matias Saldaño <matiasnsalda@gmail.com>
2022-07-27 16:12:00 -0300

    Funciones

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
 // PROGRAMA PARA DEFINIR COSTO DE ENVIO
+function calcularEnvio(){
 
-let ciudad
+    let ciudad
+    do {
+        ciudad = parseInt(prompt(`Hola ${nombre} ingrese su opcion correcta:\n\n 1.CABA \n 2.Provincia de BSAS \n 3.Otro \n 4. Volver atras`))
 
+        if (ciudad == 1){
+            alert ("El envio costará $500")
+           
+        } else if (ciudad == 2){
+            alert ("El envio costará $800")
+            
+        } else if (ciudad == 3) {
+            alert ("El envio costará $1500")
+           
+       }   else if (ciudad == 4) {
+        break
+       }else{
+        alert ("Ingrese una opción correcta")
+       }
+    
+    } while (ciudad !=4); 
+}
+
+let menu
+let nombre
 do {
-    ciudad = parseInt(prompt("Ingrese su opcion correcta:\n\n 1.CABA \n 2.Provincia de BSAS \n 3.Otro \n 4. Salir de la aplicacion"))
+    menu= parseInt(prompt("Bievenido al sistema de presupuesto de envios, elija la opcion que desee.\n\n 1.Ingresar cuenta \n 2.Empresas de envio \n 3 Consultas \n 4. Salir de la aplicacion"))
     
-    
-    if (ciudad == 1){
-        alert ("El envio costará $500")
-       
-    } else if (ciudad == 2){
-        alert ("El envio costará $800")
+    switch(menu){
+        case 1:
+            nombre=prompt("Ingrese su nombre")
+            calcularEnvio()
+            break
+        case 2:
+            alert("Las empresas que trabajan con nosotros son:\n\n Correo Argentino \n Andreani \n OCA")
+            break
         
-    } else if (ciudad == 3) {
-        alert ("El envio costará $1500")
-       
-   }   else if (ciudad == 4) {
-    break
-   }else{
-    alert ("Ingrese una opcion correctas")
-   }
+        case 3:
+            prompt("Envie su consulta y en la brevedad sera respondida.")
+            break
+    }
 
-} while (ciudad !=4); 
+} while (menu != 4); 
 
